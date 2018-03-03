44dc5c7e66bbcb48da9215a943f7a100ed41fae9
Caleb Leung <dvcs@calebleung.com>
2018-03-03 11:25:15 -0600

    deprioritize status command for now

--- src/app.py
+++ src/app.py
@@ -23,7 +23,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.change_presence(game=discord.Game(name='!status !next !live'))  
+    await client.change_presence(game=discord.Game(name='!live !next !schedule'))  
 
 @client.command()
 async def goodbot():
